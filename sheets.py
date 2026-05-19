1841d8cffa0b484e3b9ca50ebc19fd3f1abb6942
mooncookie6666 <mooncookie6666@gmail.com>
2026-05-19 15:23:04 +0300

    Create sheets.py

---
+++ sheets.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("feedback_bot").sheet1
+
+
+def save_to_sheets(date, source, review, contact):
+    sheet.append_row([date, source, review, contact])
