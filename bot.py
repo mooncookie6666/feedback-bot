b296bda9206787977fc49036f83056fee70551ae
mooncookie6666 <mooncookie6666@gmail.com>
2026-05-19 15:32:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from sheets import save_to_sheets
 
 from states import Feedback
 from keyboards import start_kb, contact_kb, restart_kb
