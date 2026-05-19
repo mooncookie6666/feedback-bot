9ad7d9c4e9ff87913bcb27ed66448e4b67a378e4
mooncookie6666 <mooncookie6666@gmail.com>
2026-05-19 14:46:02 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,3 +5,11 @@ start_kb.add(
     KeyboardButton("Гостиница"),
     KeyboardButton("Лофт")
 )
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+contact_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+contact_kb.add(
+    KeyboardButton("📱 Отправить контакт", request_contact=True),
+    KeyboardButton("❌ Пропустить")
+)
